428242197c07a13fa68492b7bed0a49920a66c21
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-01-29 11:40:34 -0700

    [cleanup] - Move Menus

---
+++ client/cl_menus.lua
@@ -0,0 +1,25 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- Walk Styles Menu --
+RegisterNetEvent('xt-walkstyles:client:Menu', function()
+    local walkStyles = {}
+
+    walkStyles[#walkStyles+1] = {
+        title = 'Refresh Current Walk Style',
+        serverEvent = 'xt-walkstyles:server:GetWalkStyle',
+    }
+
+    for r = 1, #Config.Styles, 1 do
+        walkStyles[#walkStyles+1] = {
+            title = Config.Styles[r].name,
+            event = 'xt-walkstyles:client:SetWalkStyle',
+            args = Config.Styles[r].value
+        }
+    end
+    lib.registerContext({
+        id = 'walkingstyles_menu',
+        title = 'Walking Styles',
+        options = walkStyles
+    })
+    lib.showContext('walkingstyles_menu')
+end)
