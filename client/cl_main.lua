f3fef0d5c0e5f39bb2a3617f009dc31de5dadf73
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-07-09 15:49:18 -0600

    chore(client): general cleanup + new utils

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,35 +1,9 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-local PlayerData = {}
-local currentWalk = 'default'
-
--- Set Walk Style (Send to Server & Set Metadata) --
-local function WalkStyle(walk)
-	local ped = PlayerPedId()
-	if walk == 'default' then
-		ResetPedMovementClipset(ped)
-		ResetPedWeaponMovementClipset(ped)
-		ResetPedStrafeClipset(ped)
-	else
-		RequestAnimSet(walk)
-		while not HasAnimSetLoaded(walk) do Wait(0) end
-		SetPedMovementClipset(ped, walk)
-		ResetPedWeaponMovementClipset(ped)
-		ResetPedStrafeClipset(ped)
-	end
-    TriggerServerEvent('xt-walkstyles:server:SetWalkStyle', walk)
-    XTDebug('Walk Style Changed', 'Style: '..walk)
-end
-
--- Reset Walking Style --
-local function GetWalkStyle()
-    TriggerServerEvent('xt-walkstyles:server:GetWalkStyle')
-end
-exports('GetWalkStyle', GetWalkStyle)
+local Utils = require('client.utils')
 
 -- Set Walk Style --
 RegisterNetEvent('xt-walkstyles:client:SetWalkStyle', function(walk)
     currentWalk = walk
-	WalkStyle(walk)
+	Utils.WalkStyle(walk)
 end)
 
 -- Menu Command --
@@ -41,17 +15,13 @@ end, false)
 CreateThread(function()
     while true do
         Wait(1000)
-        local walking = GetPedMovementClipset(PlayerPedId())
-        if (walking ~= joaat(currentWalk) and walking ~= joaat('move_lester_caneup') and walking ~= joaat('move_ped_crouched')) or currentWalk == "default" then -- Gets integer value of currentWalk, compares it to current clipset
-            GetWalkStyle()
+        if not Utils.IgnoredClipsets() then
+            Utils.GetWalkStyle()
             XTDebug('Reset Walk Style', 'Style: '..currentWalk)
         end
     end
 end)
 
--- Player Shit --
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function(playerData)
-	PlayerData = QBCore.Functions.GetPlayerData()
-    Wait(500)
-	GetWalkStyle()-- Get Walking Style on Player Load
-end)
+-- Player Load / Resource Start  --
+RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function() Utils.GetWalkStyle() end)
+AddEventHandler('onResourceStart', function(resource) if resource == GetCurrentResourceName() then Utils.GetWalkStyle() end end)
